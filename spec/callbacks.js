02fd2fe3f94268694bcf9a43f2ef390f891325c2
Tyler Lambe <tyler@batchacademy.com>
2016-05-21 02:30:45 -0400

    Solved

--- spec/callbacks.js
+++ spec/callbacks.js
@@ -8,7 +8,7 @@
         var animals = ['ant', 'bat', 'cat'];
         var iterationInputs = [];
 
-        _.each(animals, function(animal, index, list) {
+        each(animals, function(animal, index, list) {
           iterationInputs.push([animal, index, list]);
         });
 
@@ -25,7 +25,7 @@
 
         animals.shouldBeIgnored = 'Ignore me!';
 
-        _.each(animals, function(animal, index, list) {
+        each(animals, function(animal, index, list) {
           iterationInputs.push([animal, index, list]);
         });
 
@@ -40,7 +40,7 @@
         var animals = { a: 'ant', b: 'bat', c: 'cat' };
         var iterationInputs = [];
 
-        _.each(animals, function(animal, key, object) {
+        each(animals, function(animal, key, object) {
           iterationInputs.push([animal, key, object]);
         });
 
@@ -55,14 +55,14 @@
     describe('filter', function() {
       it('should return all even numbers in an array', function() {
         var isEven = function(num) { return num % 2 === 0; };
-        var evens = _.filter([1, 2, 3, 4, 5, 6], isEven);
+        var evens = filter([1, 2, 3, 4, 5, 6], isEven);
 
         expect(evens).to.eql([2, 4, 6]);
       });
 
       it('should return all odd numbers in an array', function() {
         var isOdd = function(num) { return num % 2 !== 0; };
-        var odds = _.filter([1, 2, 3, 4, 5, 6], isOdd);
+        var odds = filter([1, 2, 3, 4, 5, 6], isOdd);
 
         expect(odds).to.eql([1, 3, 5]);
       });
@@ -70,7 +70,7 @@
       it('should produce a brand new array instead of modifying the input array', function() {
         var isOdd = function(num) { return num % 2 !== 0; };
         var numbers = [1, 2, 3, 4, 5, 6];
-        var evens = _.filter(numbers, isOdd);
+        var evens = filter(numbers, isOdd);
 
         expect(evens).to.not.equal(numbers);
       });
@@ -78,7 +78,7 @@
 
     describe('map', function() {
       it('should apply a function to every value in an array', function() {
-        var doubledNumbers = _.map([1, 2, 3], function(num) {
+        var doubledNumbers = map([1, 2, 3], function(num) {
           return num * 2;
         });
 
@@ -87,7 +87,7 @@
 
       it('should produce a brand new array instead of modifying the input array', function() {
         var numbers = [1, 2, 3];
-        var mappedNumbers = _.map(numbers, function(num) {
+        var mappedNumbers = map(numbers, function(num) {
           return num;
         });
 
@@ -98,28 +98,28 @@
     describe('reduce', function() {
       it('should be able to sum up an array', function() {
         var add = function(tally, item) {return tally + item; };
-        var total = _.reduce([1, 2, 3], add, 0);
+        var total = reduce([1, 2, 3], add, 0);
 
         expect(total).to.equal(6);
       });
 
       it('should use the first element as an accumulator when none is given', function() {
         var add = function(tally, item) {return tally + item; };
-        var total = _.reduce([1, 2, 3], add);
+        var total = reduce([1, 2, 3], add);
 
         expect(total).to.equal(6);
       });
 
       it('should invoke the iterator on the first element when given an accumulator', function() {
         var sumSquares = function(tally, item) {return tally + item * item; };
-        var total = _.reduce([2, 3], sumSquares, 0);
+        var total = reduce([2, 3], sumSquares, 0);
 
         expect(total).to.equal(13);
       });
 
       it('should not invoke the iterator on the first element when using it as an accumulator', function() {
         var sumSquares = function(tally, item) {return tally + item * item; };
-        var total = _.reduce([2, 3], sumSquares);
+        var total = reduce([2, 3], sumSquares);
 
         expect(total).to.equal(11);
       });
